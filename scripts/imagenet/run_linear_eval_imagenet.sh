ebc8d367efc3e7686e2ea679b6ab28b6a343923f
CupidJay <34046764+CupidJay@users.noreply.github.com>
2023-03-14 10:54:01 +0800

    Update run_linear_eval_imagenet.sh

--- scripts/imagenet/run_linear_eval_imagenet.sh
+++ scripts/imagenet/run_linear_eval_imagenet.sh
@@ -1,3 +1,3 @@
 python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py \
-    linear  --config ./configs/ssl_qat/resnet18_linear_eval_imagenet.yaml \
+    linear  --config ./configs/imagenet/resnet18_linear_eval_imagenet.yaml \
     --output ./train_log_lars/resnet18_byol_4w4f_imagenet_linear_eval -j 8
